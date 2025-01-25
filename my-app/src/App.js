3793c0fe03411733365d88de397669af809c6511
Ashwini C <ashwinic@Ashwinis-MacBook-Air.local>
2025-01-25 14:48:55 +0100

    chnages for image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import textrackGif from './resources/textrack-gif.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={textrackGif} className="image" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+       test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
